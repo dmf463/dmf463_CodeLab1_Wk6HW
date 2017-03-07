78decb7732bab23288ec4ad696fa940aa35a2ddf
DavidFrisch <dmf463@nyu.edu>
2017-03-07 18:45:50 -0500

    OMG FINALLY GOT THE DAMN TEMPERATURE HALLELUJAH

--- JSON_HW/Assets/Scripts/CharacterChoiceScript.cs
+++ JSON_HW/Assets/Scripts/CharacterChoiceScript.cs
@@ -13,7 +13,7 @@ public class CharacterChoiceScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        ChooseCharacter(null, null);
+        ChooseCharacter("miami", "fl");
 		
 	}
 	
@@ -51,8 +51,12 @@ public class CharacterChoiceScript : MonoBehaviour {
     {
         WebClient client = new WebClient();
         ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
-        string content = client.DownloadString("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+        string content = client.DownloadString("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + cityName + "%2C%20" + stateCode + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
         Debug.Log(content);
+        JSONNode place = JSON.Parse(content);
+        string temp = place["query"]["results"]["channel"]["item"]["condition"]["temp"];
+        print(temp);
+        
     }
 
 
